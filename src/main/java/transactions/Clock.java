5835acccf7e8611844499facc299c1c48127e968
cverdi <cristina.verdi.tobon@gmail.com>
2018-06-25 16:21:36 +0200

    REFACTOR date format to constant

--- src/main/java/transactions/Clock.java
+++ src/main/java/transactions/Clock.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Clock {
+
+    public static final String DD_MM_YYYY = "dd/MM/yyyy";
+
     public String formattedToday() {
         LocalDate today = LocalDate.now();
-        return today.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        return today.format(DateTimeFormatter.ofPattern(DD_MM_YYYY));
     }
 }
